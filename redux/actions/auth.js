0d1adce70712dda2fffb213a35d5d26d19dc21e2
anakbaiq <116356169+anakbaiq@users.noreply.github.com>
2023-03-21 20:42:11 +0700

    Add files via upload

---
+++ redux/actions/auth.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { http } from "../../helpers/http";
+
+export const loginAction = createAsyncThunk(
+  "auth/loginAsync",
+  async ({ email, password, callback }) => {
+    try {
+      const { data }= await http.post(`/auth/login`,{
+        email,
+        password,
+      });
+      callback();
+      console.log(data.results)
+      return data.results;
+    } catch (err) {
+      // console.log(err);
+      callback(err)
+      throw err;
+    }
+  }
+);
+
+export const registerAction = createAsyncThunk(
+  "auth/registerAsync",
+  async ({ email, password,firstName,lastName,phoneNumber,callbackRegister }) => {
+    try {
+      const { data }= await http.post(`/auth/register`, {
+        email,
+        password,
+        firstName,
+        lastName,
+        phoneNumber
+      });
+      callbackRegister();
+      console.log(data[0])
+      return data[0]
+    } catch (err) {
+      console.log(err);
+      callbackRegister(err)
+      throw err;
+    }
+  }
+);
